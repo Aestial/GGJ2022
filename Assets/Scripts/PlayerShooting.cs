c7dc23e68fca8e1baad974666105033709f7c357
Jaime Hernandez <jaimevazz@gmail.com>
2022-01-21 16:05:17 -0600

    Started shooting mechanic

---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    [SerializeField] private GameObject m_BulletPrefab;
+    [SerializeField] private Transform m_Target;
+    [SerializeField] private float m_ShootInterval = 0.5f;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
